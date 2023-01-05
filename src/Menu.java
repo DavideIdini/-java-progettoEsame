d972e5a75026c9b2a536efa63eac642f7968599d
Davide <davide76.di@gmail.com>
2023-01-05 18:26:31 +0100

    aggiunto metodo wait list

--- src/Menu.java
+++ src/Menu.java
@@ -11,20 +11,17 @@ public class Menu {
             "3) Elimina appuntamento\n"+
             "4) Ricerca appuntamenti\n"+
             "5) Calcola statistiche\n"+
-            "6) Esci";
+            "6) Aggiungi persone alla Wait list\n"+
+            "7) Esci";
     public static void main(String[] args) {
-        //dichiarazione variabili
+
         Map<Integer, List<Appuntamento>> appuntamenti_per_medico = new HashMap<>();
         Agenda agenda = new Agenda(appuntamenti_per_medico);
-
-        menu(agenda);
-
-
-
-
+        List<Paziente> waitList = new ArrayList<>();
+        menu(agenda,waitList);
     }
 
-    private static void menu(Agenda agenda) {
+    private static void menu(Agenda agenda,List<Paziente> waitList) {
 
         Scanner tastiera = new Scanner(System.in);
         boolean flag = true;
@@ -33,27 +30,18 @@ public class Menu {
             int scelta = tastiera.nextInt();
             switch (scelta)
             {
-                case 1:{
-                    int a = 0;
-                    while ( a == 0)
-                    {
-
-                        Appuntamento.creaAppuntamenti(tastiera,agenda);
-                        //System.out.println(appuntamenti.get(0).toString());
-                        a++;
-                        break;
-
-                    }
-                }
+                case 1: {Appuntamento.creaAppuntamenti(tastiera,agenda);break;}
                 case 2: { agenda.modificaAppuntamento(agenda); break;}
                 case 3 : { agenda.eliminaAppuntamento(agenda); break;}
                 case 4 : { agenda.ricercaAppuntamenti(agenda); break;}
-                case 6: {exit(1);}
+                case 6 : { waitList = Paziente.JoinWaitList(waitList); break;}
+                case 7: {exit(1);}
 
             }
 
         }
     }
+
 
 
 
--- src/Paziente.java
+++ src/Paziente.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Paziente extends Persona{
     private String email;
     private String cellulare;
@@ -9,7 +12,52 @@ public class Paziente extends Persona{
         super(codiceFiscale, nome, cognome, indirizzo);
         this.email = email;
         this.cellulare = cellulare;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCellulare() {
+        return cellulare;
+    }
+
+    public void setCellulare(String cellulare) {
+        this.cellulare = cellulare;
+    }
+
+    public String getId_paziente() {
+        return id_paziente;
+    }
+
+    public void setId_paziente(String id_paziente) {
         this.id_paziente = id_paziente;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
+     public static List<Paziente> JoinWaitList(List<Paziente> waitList){
+        Scanner tastiera = new Scanner(System.in);
+        Paziente a = null;
+        System.out.println("inserisc dati paziente da aggiungere alla waitList"+"\n codice fiscale \n nome \n cognome \n indirizzo \n email \n cellulare ");
+         a.setCodiceFiscale(tastiera.nextLine());
+         a.setNome(tastiera.nextLine());
+         a.setCognome(tastiera.nextLine());
+         a.setIndirizzo(tastiera.nextLine());
+         a.setEmail(tastiera.nextLine());
+         a.setCellulare(tastiera.nextLine());
+         waitList.add(a);
+
+
+        return waitList;
+     }
 }
